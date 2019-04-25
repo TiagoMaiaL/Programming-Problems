7fd5f18080c2229ac90529788b930d6fac8540ae
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-04-25 19:12:03 -0300

    Implement findSingleInteger function, with unit tests

--- problems/numbers-appear-once/numbers-appear-once.js
+++ problems/numbers-appear-once/numbers-appear-once.js
@@ -1,7 +1,38 @@
 'use strict';
 
-function findSingleInteger() {
+/**
+ * Given an array of integers, find the integer that appears only once in the array.
+ * Note: All other integers appear exactly twice in the array, except one.
+ * @param {Array} arr - the array containing a single integer to be found.
+ * @returns {Number} - the single integer.
+ */
+function findSingleInteger(arr) {
+    if (!Array.isArray(arr)) {
+        throw new TypeError('The array must be a valid one.');
+    }
 
+    if (arr.length === 0) {
+        return undefined;
+    }
+
+    let analyzedIntegers = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const integer = arr[i];
+        
+        let pairIndex = analyzedIntegers.indexOf(integer);
+        if (pairIndex > -1) {
+            // Found it, so it's not a single integer.
+            // Remove it from the analyzed integers array.
+            analyzedIntegers.splice(pairIndex, 1);
+        } else {
+            // Add it to be analyzed.
+            analyzedIntegers.push(integer);
+        }
+    }
+
+    // By now, the array has the single integer, so return it.
+    return analyzedIntegers.shift();
 }
 
 module.exports = { findSingleInteger };
--- problems/numbers-appear-once/numbers-appear-once.test.js
+++ problems/numbers-appear-once/numbers-appear-once.test.js
@@ -3,5 +3,39 @@
 const { findSingleInteger } = require('./numbers-appear-once');
 
 describe('Numbers appear once', () => {
+    test('finding the single integer of a non-array should throw an error', () => {
+        expect(() => findSingleInteger(null)).toThrow(TypeError);
+    });
 
+    test('finding the single integer of an empty array should return undefined', () => {
+        expect(findSingleInteger([])).toBeUndefined();
+    });
+
+    test('finding the single integer of an array with one element should return it', () => {
+        expect(findSingleInteger([1])).toBe(1);
+    });
+
+    test('should find the single integer out of an array with 3 elements', () => {
+        expect(findSingleInteger([1, 1, 3])).toBe(3);
+    });
+
+    test('should find the single integer out of an array with 3 elements, with the single integer being in the beginning', () => {
+        expect(findSingleInteger([9, 2, 2])).toBe(9);
+    });
+
+    test('should find the single integer out of an array with 3 elements, with the single integer being in the middle', () => {
+        expect(findSingleInteger([2, 9, 2])).toBe(9);
+    });
+
+    test('should find the single integer out of an array with 5 elements, with the single integer being in the middle', () => {
+        expect(findSingleInteger([1, 1, 7, 2, 2])).toBe(7);
+    });
+
+    test('should find the single integer out of an array with 11 elements, with the single integer being in anywhere', () => {
+        expect(findSingleInteger([10, 4, 1, 3, 4, 1000, 2, 10, 3, 1, 2])).toBe(1000);
+    });
+
+    test('should find the single integer out of an array with 9 elements, with the single integer being in anywhere', () => {
+        expect(findSingleInteger([10, 20, 30, 40, 50, 10, 20, 40, 50])).toBe(30);
+    });
 });
