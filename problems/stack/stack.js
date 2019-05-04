792210ce6e1ed5f51a04c52a1122e8c691f27c0e
Tiago Maia <tiago.maia.lopes@gmail.com>
2019-05-04 18:54:46 -0300

    Refactor stacks to the class keyword

--- problems/stack/stack.js
+++ problems/stack/stack.js
@@ -3,105 +3,100 @@
 /**
  * A stack object.
  */
-function Stack() {
-    /**
-     * The values of the stack.
-     */
-    this.values = [];
-}
+class Stack {
 
-/**
- * Given a value, pushes it into the stack.
- * @param {Any} value
- */
-Stack.prototype.push = function(value) {
-    if (value === undefined) {
-        throw new TypeError('Value must be valid.');
+    constructor() {
+        /**
+         * The values of the stack.
+         */
+        this.values = [];
     }
 
-    this.values.push(value);
-}
+    /**
+     * Given a value, pushes it into the stack.
+     * @param {Any} value
+     */
+    push(value) {
+        if (value === undefined) {
+            throw new TypeError('Value must be valid.');
+        }
+    
+        this.values.push(value);
+    }
 
-/**
- * Returns the last valued pushed into the stack.
- * @returns {Any} the last value popped into the stack.
- */
-Stack.prototype.pop = function() {
-    return this.values.pop();
+    /**
+     * Returns the last valued pushed into the stack.
+     * @returns {Any} the last value popped into the stack.
+     */
+    pop() {
+        return this.values.pop();
+    }
 }
 
 /**
  * A stack that supports the max operation in O(1) time.
  */
-function MaxStack() {
-    Stack.call(this);
-}
+class MaxStack extends Stack {
+    
+    push(value) {
+        // Get a reference to the max and min values from the last pushed value:
+        let { max } = this.values[this.values.length - 1] || { max: value, min: value };
 
-// Inherit from stack:
-MaxStack.prototype = new Stack();
-
-MaxStack.prototype.push = function(value) {
-    // Get a reference to the max and min values from the last pushed value:
-    let { max } = this.values[this.values.length - 1] || { max: value, min: value };
-
-    // Every time we push a value, we need to wrap it in an object, so we can also keep a record
-    // of the largest and smallest values in the stack so far.
-    Stack.prototype.push.call(this, {
-        value: value,
-        max: value > max ? value : max,
-    });
-}
-
-MaxStack.prototype.pop = function() {
-    if (this.values.length > 0) {
-        return Stack.prototype.pop.apply(this).value;
+        // Every time we push a value, we need to wrap it in an object, so we can also keep a record
+        // of the largest and smallest values in the stack so far.
+        super.push({
+            value: value,
+            max: value > max ? value : max,
+        });
     }
-};
 
-/**
- * Returns the largest element pushed into the stack.
- */
-MaxStack.prototype.max = function() {
-    if (this.values.length > 0) {
-        return this.values[this.values.length - 1].max;
+    pop() {
+        if (this.values.length > 0) {
+            return Stack.prototype.pop.apply(this).value;
+        }
+    }
+
+    /**
+     * Returns the largest element pushed into the stack.
+     */
+    max() {
+        if (this.values.length > 0) {
+            return this.values[this.values.length - 1].max;
+        }
     }
 }
 
 /**
  * A stack that supports the Min and Max operations in O(1) time.
  */
-function MinMaxStack() {
-    Stack.call(this);
-}
+class MinMaxStack extends MaxStack {
 
-MinMaxStack.prototype = new MaxStack();
-
-MinMaxStack.prototype.push = function(value) {
-    let min;
-
-    // Get the last pushed value.
-    if (this.values.length > 0) {
-        const last = this.values[this.values.length - 1];
-        min = value < last.min ? value : last.min;
-    } else {
-        min = value;
-    }
+    push(value) {
+        let min;
     
-    // Reuse the push from the MaxStack.
-    MaxStack.prototype.push.call(this, value);
+        // Get the last pushed value.
+        if (this.values.length > 0) {
+            const last = this.values[this.values.length - 1];
+            min = value < last.min ? value : last.min;
+        } else {
+            min = value;
+        }
+        
+        // Reuse the push from the MaxStack.
+        super.push(value);
+    
+        // Get the new pushed one and add the min to it.
+        this.values[this.values.length - 1].min = min;
+    }
 
-    // Get the new pushed one and add the min to it.
-    this.values[this.values.length - 1].min = min;
-}
-
-/**
- * Returns the smallest element pushed into the stack.
- */
-MinMaxStack.prototype.min = function() {
-    if (this.values.length > 0) {
-        return this.values[this.values.length - 1].min;
+    /**
+     * Returns the smallest element pushed into the stack.
+     */
+    min() {
+        if (this.values.length > 0) {
+            return this.values[this.values.length - 1].min;
+        }
     }
 }
 
 module.exports = { Stack, MaxStack, MinMaxStack };
-
